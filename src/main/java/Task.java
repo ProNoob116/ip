e67336abb7d7b33a6a12f09c1c5390e7bde65ccc
Satya Venkat Pranav <gsvpranav@gmail.com>
2026-08-26 12:48:11 +0800

    Add Task class and mark/unmark functionality

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false; // Tasks start as not done
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // Returns "X" if done, or a blank space if not
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
--- src/main/java/Wallis.java
+++ src/main/java/Wallis.java
@@ -4,14 +4,13 @@ public class Wallis {
     public static void main(String[] args) {
         String botName = "Wallis";
         
-        // Greet
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + botName);
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
         
-        // Task memory
-        String[] tasks = new String[100];
+        // We now use an array of Task objects instead of Strings
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         
         Scanner scanner = new Scanner(System.in);
@@ -24,12 +23,31 @@ public class Wallis {
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i].toString());
                 }
                 System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("mark ")) {
+                // Extract the number from the command (e.g., "mark 2" -> index 1)
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                tasks[index].markAsDone();
+                
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[index].toString());
+                System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("unmark ")) {
+                // Extract the number from the command (e.g., "unmark 2" -> index 1)
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks[index].unmarkAsDone();
+                
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[index].toString());
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 
                 System.out.println("____________________________________________________________");
@@ -38,7 +56,6 @@ public class Wallis {
             }
         }
         
-        // Exit
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
