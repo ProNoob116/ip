5a7366a57c918f18fef288024e782f47bd73391a
Satya Venkat Pranav <gsvpranav@gmail.com>
2026-08-26 12:31:50 +0800

    Add Increment Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        System.out.println("Hello from\n" + logo);
+        
+        // --- LEVEL 0: Rename, Greet, Exit ---
+        String botName = "Wallis";
+        
+        // Greet
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + botName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        
+        // Exit
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
